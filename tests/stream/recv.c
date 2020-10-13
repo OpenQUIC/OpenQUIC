465eff7d39a5f6c8d13a20c467cff34caac861d2
Gscienty <gaoxiaochuan@hotmail.com>
2020-10-13 21:48:52 +0800

    test: 模块化stream读数据功能

--- tests/stream/recv.c
+++ tests/stream/recv.c
@@ -1,22 +1,23 @@
-#include "stream.h"
+#include "module.h"
+#include "modules/stream.h"
+#include "recovery/flowctrl.h"
 #include "liteco.h"
 #include <pthread.h>
 #include <unistd.h>
 
-bool abandon(quic_flowctrl_t *const flowctrl) {
+bool abandon(quic_stream_flowctrl_t *const flowctrl) {
     (void) flowctrl;
-
 
     return true;
 }
 
-void update_rwnd(quic_flowctrl_t *const flowctrl, const uint64_t rwnd, const bool fin) {
+void update_rwnd(quic_stream_flowctrl_t *const flowctrl, const uint64_t rwnd, const bool fin) {
     (void) flowctrl;
     (void) rwnd;
     (void) fin;
 }
 
-quic_recv_stream_t str;
+quic_recv_stream_t *str;
 liteco_channel_t channel;
 
 liteco_runtime_t rt;
@@ -41,10 +42,10 @@ void *thread(void *const args) {
     memcpy(frame2->data, (uint8_t *) line + 5, sizeof(line) - 5);
 
     sleep(3);
-    quic_recv_stream_handle_frame(&str, frame1);
+    quic_recv_stream_handle_frame(str, frame1);
 
     sleep(3);
-    quic_recv_stream_handle_frame(&str, frame2);
+    quic_recv_stream_handle_frame(str, frame2);
 
 
     return NULL;
@@ -56,23 +57,53 @@ int quic_handle_co(void *const args) {
     return 0;
 }
 
-quic_flowctrl_module_t quic_flowctrl_module = {
+quic_stream_flowctrl_module_t quic_flowctrl_module = {
     .abandon = abandon,
     .update_rwnd = update_rwnd
 };
 
+
+quic_module_t quic_connection_flowctrl_module = {
+    .module_size = 0,
+    .init        = NULL,
+    .destory     = NULL
+};
+
+quic_err_t quic_stream_flowctrl_module_init(void *const module, quic_session_t *const sess) {
+    (void) sess;
+
+    quic_stream_flowctrl_module_t *const ref = module;
+    ref->init = NULL;
+    ref->abandon = abandon;
+    ref->update_rwnd = update_rwnd;
+
+    return quic_err_success;
+}
+
+quic_module_t quic_stream_flowctrl_module = {
+    .module_size = sizeof(quic_stream_flowctrl_module_t),
+    .init        = quic_stream_flowctrl_module_init,
+    .destory     = NULL
+};
+
 int main() {
+    quic_buf_t src = { .buf = "1", .capa = 1 };
+    quic_buf_t dst = { .buf = "1", .capa = 1 };
+    quic_session_t *session = quic_session_create(src, dst);
+
+
+    quic_stream_t *stream = quic_stream_create(1, session, &channel, &channel);
+    str = &stream->recv;
+
     pthread_t pthread;
 
     liteco_channel_init(&channel);
     liteco_runtime_init(&rt);
 
-    quic_recv_stream_init(&str, 1, NULL, &channel);
-
     pthread_create(&pthread, NULL, thread, NULL);
 
     uint8_t line[255];
-    uint64_t len = quic_recv_stream_read(&str, sizeof(line), line);
+    uint64_t len = quic_recv_stream_read(str, sizeof(line), line);
 
     printf("%ld %s\n", len, line);
 
