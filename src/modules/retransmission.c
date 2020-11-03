d4f5a7c6490e596a7e5e8f1c38540b16fc45e510
Gscienty <gaoxiaochuan@hotmail.com>
2020-11-03 08:39:11 +0800

    process newly acked packet

--- src/modules/retransmission.c
+++ src/modules/retransmission.c
@@ -10,6 +10,43 @@
 
 static quic_err_t quic_retransmission_module_init(void *const module);
 
+quic_err_t quic_retransmission_module_find_newly_acked(quic_retransmission_module_t *const module, quic_frame_ack_t *const frame) {
+    quic_sent_packet_rbt_t *pkt = NULL;
+
+    quic_link_init(&module->del_mem_queue);
+
+    {
+        quic_rbt_foreach(pkt, module->sent_mem) {
+            uint64_t end = frame->largest_ack;
+            uint64_t start = end - frame->first_range;
+
+            bool lost_flag = frame->ranges.count != 0;
+            uint32_t i = 0;
+            do {
+                if (start <= pkt->key && pkt->key <= end) {
+                    quic_retransmission_process_newly_acked(module, pkt);
+                }
+
+                if (lost_flag) {
+                    end = start - quic_arr(&frame->ranges, i, quic_ack_range_t)->gap - 2;
+                    start = end - quic_arr(&frame->ranges, i, quic_ack_range_t)->len;
+                }
+            } while (++i < frame->ranges.count);
+        }
+    }
+
+    while (!quic_link_empty(&module->del_mem_queue)) {
+        quic_rbt_foreach_qnode_t *node = (quic_rbt_foreach_qnode_t *) quic_link_next(&module->del_mem_queue);
+        quic_sent_packet_rbt_t *pkt = (quic_sent_packet_rbt_t *) node->node;
+        quic_link_remove(node);
+        free(node);
+
+        quic_retransmission_sent_mem_drop(module, pkt);
+    }
+
+    return quic_err_success;
+}
+
 static quic_err_t quic_retransmission_module_init(void *const module) {
     quic_retransmission_module_t *r_module = (quic_retransmission_module_t *) module;
 
--- src/modules/retransmission.h
+++ src/modules/retransmission.h
@@ -10,6 +10,7 @@
 #define __OPENQUIC_RETRANSMISSION_H__
 
 #include "module.h"
+#include "format/frame.h"
 #include "utils/rbt.h"
 #include "utils/link.h"
 
@@ -36,11 +37,15 @@ struct quic_retransmission_module_s {
     uint32_t sent_count;
 
     uint32_t unacked_len;
+
+    quic_link_t del_mem_queue;
 };
 
 extern quic_module_t quic_initial_retransmission_module;
 extern quic_module_t quic_handshake_retransmission_module;
 extern quic_module_t quic_app_retransmission_module;
+
+quic_err_t quic_retransmission_module_find_newly_acked(quic_retransmission_module_t *const module, quic_frame_ack_t *const frame);
 
 static inline quic_err_t quic_retransmission_sent_mem_push(quic_retransmission_module_t *const module, quic_sent_packet_rbt_t *const pkt) {
     quic_sent_pkts_insert(&module->sent_mem, pkt);
@@ -61,6 +66,23 @@ static inline quic_err_t quic_retransmission_sent_mem_drop(quic_retransmission_m
         free(node);
     }
     free(pkt);
+
+    return quic_err_success;
+}
+
+static inline quic_err_t quic_retransmission_process_newly_acked(quic_retransmission_module_t *const module, quic_sent_packet_rbt_t *const pkt) {
+    quic_rbt_foreach_qnode_t *node = malloc(sizeof(quic_rbt_foreach_qnode_t));
+    if (node == NULL) {
+        return quic_err_internal_error;
+    }
+    node->node = (quic_rbt_t *) pkt;
+    quic_link_insert_after(&module->del_mem_queue, node);
+
+    if (pkt->included_unacked) {
+        module->unacked_len -= pkt->pkt_len;
+
+        // TODO congestion on packet acked and detect lost packet alerm
+    }
 
     return quic_err_success;
 }
