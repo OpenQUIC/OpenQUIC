f06069b59f5ff51fc5e393f6dc348c30aa918799
Gscienty <gaoxiaochuan@hotmail.com>
2020-12-03 00:10:22 +0800

    feat: use morris binary tree travel (used for rbt_foreach)

--- src/utils/rbt.h
+++ src/utils/rbt.h
@@ -17,10 +17,11 @@
 #define QUIC_RBT_BLACK  0
 #define QUIC_RBT_RED    1
 
-#define QUIC_RBT_FIELDS \
-    quic_rbt_t *rb_p;   \
-    quic_rbt_t *rb_r;   \
-    quic_rbt_t *rb_l;   \
+#define QUIC_RBT_FIELDS      \
+    quic_rbt_t *rb_p;        \
+    quic_rbt_t *rb_r;        \
+    quic_rbt_t *rb_l;        \
+    quic_rbt_t *mirris_link; \
     uint8_t rb_color;
 
 #define QUIC_RBT_EQ 0
@@ -32,26 +33,33 @@ struct quic_rbt_s {
     QUIC_RBT_FIELDS
 };
 
+extern const quic_rbt_t rbt_nil;
+#define quic_rbt_nil ((quic_rbt_t *) &rbt_nil)
+
+#define quic_rbt_is_nil(node)                   \
+    (((quic_rbt_t *) (node)) == quic_rbt_nil)
+
 #define quic_rbt_init(node) {           \
     (node)->rb_p = quic_rbt_nil;        \
     (node)->rb_r = quic_rbt_nil;        \
     (node)->rb_l = quic_rbt_nil;        \
+    (node)->mirris_link = quic_rbt_nil; \
     (node)->rb_color = QUIC_RBT_RED;    \
 }
 
-#define quic_rbt_tree_init(root)            \
+#define quic_rbt_tree_init(root) \
     (root) = (typeof(root)) quic_rbt_nil
 
 typedef int (*quic_rbt_comparer_t) (const quic_rbt_t *const lf, const quic_rbt_t *const rt);
 typedef int (*quic_rbt_key_comparer_t) (const void *const key, const quic_rbt_t *const node);
 
-#define quic_rbt_insert(root, node, comparer)                                           \
+#define quic_rbt_insert(root, node, comparer) \
     quic_rbt_insert_inner((quic_rbt_t **) (root), (quic_rbt_t *) (node), (comparer))
 quic_err_t quic_rbt_insert_inner(quic_rbt_t **const root, quic_rbt_t *const node, quic_rbt_comparer_t comparer);
-#define quic_rbt_remove(root, node)                                                     \
+#define quic_rbt_remove(root, node) \
     quic_rbt_remove_inner((quic_rbt_t **) (root), (quic_rbt_t **) (node))
 quic_err_t quic_rbt_remove_inner(quic_rbt_t **const root, quic_rbt_t **const node);
-#define quic_rbt_find(root, key, comparer)                                              \
+#define quic_rbt_find(root, key, comparer) \
     quic_rbt_find_inner((quic_rbt_t *) (root), (const void *) (key), (comparer))
 quic_rbt_t *quic_rbt_find_inner(quic_rbt_t *const root, const void *const key, quic_rbt_key_comparer_t comparer);
 
@@ -62,43 +70,83 @@ struct quic_rbt_foreach_qnode_s {
     quic_rbt_t *node;
 };
 
-// TODO 第一次循环时，重复获取根节点
-#define quic_rbt_foreach(node_, root)                                                                              \
-    quic_link_t _rbt_foreach_queue;                                                                                \
-    quic_link_init(&_rbt_foreach_queue);                                                                           \
-    for (({ \
-            (node_) = (typeof((node_))) (root); \
-          }); \
-          !quic_rbt_is_nil((node_)); \
-         ({ \
-            if (!quic_rbt_is_nil((node_)->rb_r)) { \
-                quic_rbt_foreach_qnode_t *newly_node = malloc(sizeof(quic_rbt_foreach_qnode_t)); \
-                quic_link_init(newly_node); \
-                newly_node->node = (node_)->rb_r; \
-                quic_link_insert_after(&_rbt_foreach_queue, newly_node); \
-            } \
-            if (!quic_rbt_is_nil((node_)->rb_l)) { \
-                quic_rbt_foreach_qnode_t *newly_node = malloc(sizeof(quic_rbt_foreach_qnode_t)); \
-                quic_link_init(newly_node); \
-                newly_node->node = (node_)->rb_l; \
-                quic_link_insert_after(&_rbt_foreach_queue, newly_node); \
-            } \
-            if (quic_link_empty(&_rbt_foreach_queue)) { \
-                (node_) = (typeof((node_))) quic_rbt_nil; \
-            } \
-            else { \
-                quic_rbt_foreach_qnode_t *cur_node = (typeof(cur_node)) quic_link_prev(&_rbt_foreach_queue); \
-                quic_link_remove(cur_node); \
-                (node_) = (typeof((node_))) cur_node->node; \
-                free(cur_node); \
-            } \
-          }))
+// Morris Binary Tree Travel
+typedef struct quic_rbt_iterator_s quic_rbt_iterator_t;
+struct quic_rbt_iterator_s {
+    bool interrupt_1;
+    bool interrupt_2;
 
-extern const quic_rbt_t rbt_nil;
-#define quic_rbt_nil ((quic_rbt_t *) &rbt_nil)
+    quic_rbt_t *cur;
+    quic_rbt_t *mr;
+};
 
-#define quic_rbt_is_nil(node)                   \
-    (((quic_rbt_t *) (node)) == quic_rbt_nil)
+static inline bool quic_rbt_iterator_is_end(quic_rbt_iterator_t *const iter) {
+    return quic_rbt_is_nil(iter->cur);
+}
+
+static void quic_rbt_iterator_next(quic_rbt_iterator_t *const iter) {
+#define __right(node) (quic_rbt_is_nil((node)->rb_r) ? (node)->mirris_link : (node)->rb_r)
+#define __left(node) (node)->rb_l
+    if (iter->interrupt_1) {
+        goto continue_travel_1;
+    }
+    if (iter->interrupt_2) {
+        goto continue_travel_2;
+    }
+
+    while (!quic_rbt_is_nil(iter->cur)) {
+
+        if (quic_rbt_is_nil(__left(iter->cur))) {
+            iter->interrupt_1 = true;
+            return;
+
+continue_travel_1:
+            iter->interrupt_1 = false;
+            iter->cur = __right(iter->cur);
+        }
+        else {
+            iter->mr = __left(iter->cur);
+            while (!quic_rbt_is_nil(__right(iter->mr)) && __right(iter->mr) != iter->cur) {
+                iter->mr = __right(iter->mr);
+            }
+
+            if (quic_rbt_is_nil(__right(iter->mr))) {
+                iter->mr->mirris_link = iter->cur;
+                iter->cur = __left(iter->cur);
+            }
+            else {
+                iter->interrupt_2 = true;
+                return;
+
+continue_travel_2:
+                iter->interrupt_2 = false;
+                iter->cur = __right(iter->mr);
+                iter->mr->mirris_link = quic_rbt_nil;
+                iter->cur = __right(iter->cur);
+            }
+        }
+    }
+
+#undef __right
+#undef __left
+}
+
+static inline quic_err_t quic_rbt_iterator_init(quic_rbt_iterator_t *const iter, quic_rbt_t *const root) {
+    iter->interrupt_1 = false;
+    iter->interrupt_2 = false;
+    iter->cur = root;
+    iter->mr = quic_rbt_nil;
+    quic_rbt_iterator_next(iter);
+
+    return quic_err_success;
+}
+
+#define quic_rbt_foreach(node, root)                                                                        \
+    quic_rbt_iterator_t _iter;                                                                              \
+    for (({ quic_rbt_iterator_init(&_iter, (quic_rbt_t *) (root)); (node) = (typeof((node))) _iter.cur; }); \
+         !quic_rbt_is_nil((node));                                                                          \
+         ({ quic_rbt_iterator_next(&_iter); (node) = (typeof((node))) _iter.cur; }))
+
 
 #define QUIC_RBT_UINT64_FIELDS  \
     QUIC_RBT_FIELDS             \
--- tests/utils/rbt_foreach.c
+++ tests/utils/rbt_foreach.c
@@ -1,5 +1,4 @@
 #include "utils/rbt.h"
-#include "recovery/flowctrl.h"
 #include <malloc.h>
 
 typedef struct quic_rbt_test_s quic_rbt_test_t;
@@ -7,61 +6,20 @@ struct quic_rbt_test_s {
     QUIC_RBT_UINT64_FIELDS
 };
 
-uint64_t get_swnd(quic_stream_flowctrl_t *const flowctrl) {
-    (void) flowctrl;
-    return 13;
-}
-
-void sent(quic_stream_flowctrl_t *const flowctrl, const uint64_t bytes) {
-    (void) flowctrl;
-    (void) bytes;
-}
-
-void update_rwnd(quic_stream_flowctrl_t *const flowctrl, const uint64_t t_off, const bool fin) {
-    (void) flowctrl;
-    (void) t_off;
-    (void) fin;
-}
-
-quic_err_t quic_stream_flowctrl_module_init(void *const module) {
-    quic_stream_flowctrl_module_t *const ref = module;
-    ref->init = NULL;
-    ref->get_swnd = get_swnd;
-    ref->sent = sent;
-    ref->update_rwnd = update_rwnd;
-
-    return quic_err_success;
-}
-
-quic_module_t quic_stream_flowctrl_module = {
-    .module_size = sizeof(quic_stream_flowctrl_module_t),
-    .init        = quic_stream_flowctrl_module_init,
-    .destory     = NULL
-};
-
-quic_module_t quic_connection_flowctrl_module = {
-    .module_size = 0,
-    .init = NULL,
-    .destory = NULL
-};
-
 int main() {
     quic_rbt_t *root = quic_rbt_nil;
 
     int i;
-    for (i = 0; i < 255; i++) {
+    for (i = 0; i < 999; i++) {
         quic_rbt_test_t *test1 = malloc(sizeof(quic_rbt_test_t));
         quic_rbt_init(test1);
         test1->key = i;
 
         quic_rbt_insert(&root, test1, quic_rbt_uint64_comparer);
     }
-
-    quic_rbt_test_t *node;
-    {
-        quic_rbt_foreach(node, root) {
-            printf("key: %ld\n", node->key);
-        }
+    quic_rbt_test_t *iter = NULL;
+    quic_rbt_foreach(iter, root) {
+        printf("%ld\n", iter->key);
     }
 
     return 0;
