1424b4f28822862345294716210c41852aff8ad9
Gscienty <gaoxiaochuan@hotmail.com>
2020-12-04 20:38:36 +0800

    update sorter

--- src/sorter.h
+++ src/sorter.h
@@ -13,6 +13,7 @@
 #include "utils/errno.h"
 #include "utils/rbt.h"
 #include <stdint.h>
+#include <pthread.h>
 
 #ifndef QUIC_SORTER_CLUSTER_SIZE
 #define QUIC_SORTER_CLUSTER_SIZE 4096
@@ -61,7 +62,7 @@ quic_err_t quic_sorter_destory(quic_sorter_t *const sorter);
 quic_err_t quic_sorter_write(quic_sorter_t *const sorter, uint64_t off, uint64_t len, const void *data);
 uint64_t quic_sorter_read(quic_sorter_t *const sorter, uint64_t len, void *data);
 
-#define quic_sorter_readable(sorter)                \
+#define quic_sorter_readable(sorter) \
     ((sorter)->avail_size - (sorter)->readed_size)
 
 #endif
